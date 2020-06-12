a333a26e8558b99f82b663bdf57ba6624d832c05
wilson coronado barrantes <w.coronadobarrantes@gmail.com>
2020-06-12 18:31:19 -0500

    Add Menu

---
+++ Frontend/webrossini/src/containers/Kanbanboard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+
+const KanbanBoard = () => {
+    return (
+        <div>
+            <Typography variant="h1" component="h2" gutterBottom>
+                Procesos
+            </Typography>
+        </div>
+    );
+};
+
+export default KanbanBoard;
